70674409cc3c0133f182d90fd09c87cdcb99c72e
Jianyu <jianyu-m@users.noreply.github.com>
2016-10-31 23:58:50 +0800

    Update sequence.h

--- Assignment3/sequence.h
+++ Assignment3/sequence.h
@@ -5,42 +5,23 @@ using namespace std;
 
 template <typename T>
 class Sequence {
-public:
+private:
   vector<T> arr;
   int len;
+public:
   Sequence():len(0){}
+  
+  template <typename U>
+  friend istream& operator>>(istream& in, Sequence<U>& seq);
+  
+  template <typename U>
+  friend ostream& operator<<(ostream& out, const Sequence<U>& seq);
+  
+  template <typename U>
+  friend ostream& operator<<(ostream& out, const const std::vector<Sequence<U>>& seq);
+  
+  template <typename U>
+  friend sort_list(vector<Sequence<U>>& seq);
 };
 
-template <typename T>
-istream& operator>>(istream& in, Sequence<T>& seq) {
-  int m;
-  T t;
-  in >> m;
-  for (int i = 0;i < m;i++) {
-    in >> t;
-    seq.arr.push_back(t);
-  }
-}
-
-template <typename T>
-ostream& operator<<(ostream& out, const Sequence<T>& seq) {
-  for (int i = 0;i < seq.len;i++) {
-    out << seq.arr[i] << " ";
-  }
-  out << "\n";
-}
-
-template <typename T>
-ostream& operator<<(ostream& out, const std::vector<Sequence<T>> seq) {
-  typename std::vector<Sequence<T>>::const_iterator itr;
-  for (itr = seq.begin();itr != seq.end();
-        itr++) {
-          cout << *itr;
-        }
-}
-
-template <typename T>
-void sort_list(vector<Sequence<T>> &seq) {
-
-}
 /* Other Neccessary Defination Here */
