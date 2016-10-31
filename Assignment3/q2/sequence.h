12f3aa950b56c4143e1480a0e42772383650680c
jianyu <jianyu@hku.hk>
2016-11-01 00:56:22 +0800

    final

--- Assignment3/q2/sequence.h
+++ Assignment3/q2/sequence.h
@@ -11,6 +11,8 @@ private:
   int len;
 public:
   Sequence():len(0){}
+  template <typename U>
+  friend bool seq_comp(const Sequence<U>& a, const Sequence<U>& b);  
   
   template <typename U>
   friend istream& operator>>(istream& in, Sequence<U>& seq);
@@ -27,7 +29,7 @@ istream& operator>>(istream& in, Sequence<T>& seq) {
   seq.len = m;
   for (int i = 0;i < m;i++) {
    in >> t; 
-   seq.push_back(t);
+   seq.arr.push_back(t);
   }
   return in;
 }
@@ -35,7 +37,7 @@ istream& operator>>(istream& in, Sequence<T>& seq) {
 template <typename T>
 ostream& operator<<(ostream& out, const Sequence<T>& seq) {
   for (int i= 0;i < seq.len;i++) {
-   out << seq[i] << " "; 
+   out << seq.arr[i] << " "; 
   }
   out << "\n";
   return out;
@@ -51,13 +53,23 @@ ostream& operator<<(ostream& out, const std::vector<Sequence<T>>& seq) {
 }
 
 template <typename T>
-bool seq_comp(Sequence<T>& a, Sequence<T>& b){
- return true;
+bool seq_comp(const Sequence<T>& a,const Sequence<T>& b){
+ if (a.len != b.len) {
+    return a.len < b.len;
+ }
+ T t1, t2;
+ t1 = 0;
+ t2 = 0;
+ for (int i = 0;i < a.len;i++) {
+    t1 += a.arr[i] * a.arr[i];
+    t2 += b.arr[i] * b.arr[i];
+ }
+ return t1 < t2;
 }
 
 template <typename T>
 void sort_list(vector<Sequence<T>>& seq) {
-  sort(seq.begin(), seq.end(), seq_comp);
+  sort(seq.begin(), seq.end(), seq_comp<T>);
 }
 
 /* Other Neccessary Defination Here */
