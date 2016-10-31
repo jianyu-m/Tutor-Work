c9ca45fa20d6f4113b14676d4f332d56dbf2bab9
Jianyu <jianyu-m@users.noreply.github.com>
2016-11-01 00:22:54 +0800

    Update sequence.h

--- Assignment3/sequence.h
+++ Assignment3/sequence.h
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 template <typename T>
@@ -19,9 +20,44 @@ public:
 };
 
 template <typename T>
-ostream& operator<<(ostream& out, const std::vector<Sequence<T>>& seq);
+istream& operator>>(istream& in, Sequence<T>& seq) {
+  int m;
+  T t;
+  in >> m;
+  seq.len = m;
+  for (int i = 0;i < m;i++) {
+   in >> t; 
+   seq.push_back(t);
+  }
+  return in;
+}
 
 template <typename T>
-void sort_list(vector<Sequence<T>>& seq);
+ostream& operator<<(ostream& out, const Sequence<T>& seq) {
+  for (int i= 0;i < seq.len;i++) {
+   out << seq[i] << " "; 
+  }
+  out << "\n";
+  return out;
+}
+
+template <typename T>
+ostream& operator<<(ostream& out, const std::vector<Sequence<T>>& seq) {
+ typename std::vector<Sequence<T>>::const_iterator itr;
+ for (itr = seq.begin();itr != seq.end();itr++) {
+  out << *itr; 
+ }
+  return out;
+}
+
+template <typename T>
+bool seq_comp(Sequence<T>& a, Sequence<T>& b){
+ return true;
+}
+
+template <typename T>
+void sort_list(vector<Sequence<T>>& seq) {
+  sort(seq.begin(), seq.end(), seq_comp);
+}
 
 /* Other Neccessary Defination Here */
